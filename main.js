5c7f7031d08ccde31549b45dd54e52a17d0ffb45
sukalas <litridis@gmail.com>
2017-08-11 19:20:05 +0200

    styling

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 var main = function(){
 	var windowHeight = window.innerHeight;
+	var windowWidth = window.innerWidth;
+	var vissibility = 1;
 
-
-	var init = function(param){
+	var init = function(width,height){
 		var rowList = document.getElementsByClassName("row");
+		var projects = document.getElementsByClassName("projects");
+		
 		for (let value of rowList) {
-		  value.style.height = param/3+"px";
+		  value.style.height = height/3+"px";
+		}
+		for (let value of projects) {
+			(width<770)?vissibility=0:vissibility=1;
+			value.style.opacity = vissibility;
 		}
 	}
 
-	window.onresize = function(e) {
-		var wh = window.innerHeight
-		init(wh);
+	window.onresize = function(e){
+		var wh = window.innerHeight;
+		var ww = window.innerWidth;
+		init(ww, wh);
 	};
 
-	init(windowHeight);
+	init(windowWidth, windowHeight);
 }
 
