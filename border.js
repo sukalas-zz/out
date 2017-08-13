6305b2f2166a8cc56a3e5fd4b640035a08744c0c
sukalas <litridis@gmail.com>
2017-08-13 11:55:50 +0200

    applied canvas

---
+++ border.js
@@ -0,0 +1,26 @@
+class Border {
+	constructor(posX, posY, ctx) {
+		this.posX = posX;
+		this.posY = posY;
+		this.speed = 1;
+		this.height = this.posY;
+		this.width = this.posX;
+		this.ctx = ctx;
+	}
+	draw(){
+
+		if(Math.random() < .025){
+			this.posY = Math.random() * this.height;
+			this.posX = Math.random() * this.width;
+		}
+
+		this.ctx.beginPath();
+		this.ctx.moveTo(this.width,this.posY-20);
+		this.ctx.lineTo(this.width,this.posY);
+		this.ctx.moveTo(this.posX-20,this.height-5);
+		this.ctx.lineTo(this.posX,this.height-5);
+		this.ctx.strokeStyle="white";
+		this.ctx.stroke();
+	}
+}
+
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var main = function(){
-	var windowHeight = window.innerHeight;
 	var windowWidth = window.innerWidth;
+	var windowHeight = window.innerHeight;
+
 	var vissibility = 1;
 
-	var init = function(width,height){
+	var init = function(width, height){
 		var rowList = document.getElementsByClassName("row");
 		var projects = document.getElementsByClassName("projects");
 		
@@ -17,11 +18,55 @@ var main = function(){
 	}
 
 	window.onresize = function(e){
-		var wh = window.innerHeight;
 		var ww = window.innerWidth;
+		var wh = window.innerHeight;
+		windowWidth = ww;
+		windowHeight = wh;
 		init(ww, wh);
 	};
 
 	init(windowWidth, windowHeight);
+
+	var setup = function(){
+		canvas = document.createElement('canvas');
+		ctx = canvas.getContext('2d');
+
+		canvas.width = windowWidth/3;
+		canvas.height = windowHeight/3;
+		canvas.id = 'canvas';
+
+		canvas2 = canvas.cloneNode(true);
+		canvas3 = canvas.cloneNode(true);
+		
+		ctx2 = canvas2.getContext('2d');		
+		ctx3 = canvas3.getContext('2d');
+
+		document.getElementById("bio").appendChild(canvas);
+		document.getElementById("xp").appendChild(canvas2);
+		document.getElementById("contact").appendChild(canvas3);
+
+		line = new Border(canvas.width, canvas.height, ctx);
+		line2 = new Border(canvas2.width, canvas2.height, ctx2);
+		line3 = new Border(canvas2.width, canvas2.height, ctx3);
+	}
+
+	setup();
+
+	function loop(){
+		clear();
+		line.draw();
+		line2.draw();
+		line3.draw();
+		requestAnimationFrame(loop, 1);
+	}
+
+	function clear(){
+		ctx.clearRect(0, 0, windowWidth/3, windowHeight/3);
+		ctx2.clearRect(0, 0, windowWidth/3, windowHeight/3);
+		ctx3.clearRect(0, 0, windowWidth/3, windowHeight/3);
+	}
+
+	loop();
+
 }
 
